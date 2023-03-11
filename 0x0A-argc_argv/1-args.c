4d75195f881c92ff10470a81098ffaa8fb7aaeb9
dhokabeatz <amglna2020@gmail.com>
2023-03-10 20:08:09 -0800

    arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - the function to find args
+ * @argc: Argument count
+ * @argv: Argument vector
+ * Return:Always 0
+ */
+
+int main(int argc, char __attribute__((__unused__)) *argv[])
+{
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
