d6f6453e596c94138118ec4b5ecce8be7527dc9b
dhokabeatz <amglna2020@gmail.com>
2023-03-31 19:12:07 -0700

    Added singly linked list

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * first - function executed before main
+ * Return: no return.
+ */
+void __attribute__ ((constructor)) first()
+{
+	printf("You're beat! and yet, you must allow");
+	printf(",\nI bore my house upon my back!\n");
+}
