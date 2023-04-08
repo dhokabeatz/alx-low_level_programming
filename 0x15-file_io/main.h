79d9c547d3c086b4c049261975122b3f559b87f9
dhokabeatz <amglna2020@gmail.com>
2023-04-08 09:05:14 -0700

    Added main.h file

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+
+
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+ssize_t read_textfile(const char *filename, size_t letters);
+#endif
