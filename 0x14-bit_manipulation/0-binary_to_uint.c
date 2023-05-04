082e8e8a0a1e67e0ada343f52a95b398801b9027
dhokabeatz <amglna2020@gmail.com>
2023-05-04 11:01:00 -0700

    I habe a dream

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,6 +1,13 @@
 #include "main.h"
+/**
+*
+* binary_to_uint - converts number in base two to an uinsigned int.
+* @n: pointer to a string containing the number in base 2
+*
+* Return: unsigned int with a decimal value of number in base 2 or 0 if error 
+*/ 
 
-unsigned int binary_to_uint(const char *n)
+unsigned int binary_to_uint(const char *b)
 {
 	int counter;
 	unsigned int binary_number;
@@ -8,15 +15,15 @@ unsigned int binary_to_uint(const char *n)
 	binary_number = 0;
 	if (!n)
 		return (0);
-	for (counter = 0; n[counter] != '\0'; counter++)
+	for (counter = 0; b[counter] != '\0'; counter++)
 	{
-		if (n[counter] != '0' && n[counter] != '1')
+		if (b[counter] != '0' && b[counter] != '1')
 			return (0);
 	}
-	for (counter = 0; n[counter] != '\0'; counter++)
+	for (counter = 0; b[counter] != '\0'; counter++)
 	{
 		binary_number <<= 1;
-		if (n[counter] == '1')
+		if (b[counter] == '1')
 			binary_number += 1;
 	}
 	return (binary_number);
