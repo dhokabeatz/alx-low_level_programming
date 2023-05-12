d1588c114c96abd421747a5b503a19df68c40cae
dhokabeatz <amglna2020@gmail.com>
2023-05-12 11:46:02 -0700

    Other codes conflicting checks

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,46 +1,41 @@
 #include "main.h"
 
 /**
-*read_textfile - reads a text file and prins it to the terminal
-*@filename: name of the file to be read
-*@letter: number of letters it should read
-*Return: actual number of letters it could read and print at the terminal
-*/
-
+ * read_textfile - Reads a text file and prints to POSIX STDOUT
+ * @filename: The filename to open
+ * @letters: The number of letters to read and print
+ * Return: The number of letters read and printed, or 0 on failure
+ */
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int file_data;
-	ssize_t len;
-	ssize_t len_w;
-	char *buffer;
+	int fd, r, w;
+	char *buff;
 
 	if (filename == NULL)
+		return (0);
+
+	buff = malloc(sizeof(char) * letters);
+
+	if (buff == NULL)
 	{
+		free(buff);
 		return (0);
 	}
-	file_data = open(filename, O_RDONLY);
-	if (file_data == -1)
+	fd = open(filename, O_RDONLY);
+	r = read(fd, buff, letters);
+
+	if (fd == -1 || r == -1)
 	{
+		free(buff);
 		return (0);
 	}
-	buffer = malloc(sizeof(char) * letters);
-	if (buffer == NULL)
-	{
-		close(file_data);
+
+	w = write(STDOUT_FILENO, buff, r);
+	free(buff);
+	close(fd);
+
+	if (w == -1)
 		return (0);
-	}
-	len = read(file_data, buffer, letters);
-	if (len == -1)
-	{
-		free(buffer);
-		return (0);
-	}
-	len_w = write(STDOUT_FILENO, buffer, len);
-	free(buffer);
-	if (len != len_w)
-	{
-		return (0);
-	}
-	return (len_w);
+	return (w);
 }
