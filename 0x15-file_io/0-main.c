61b401b9515c8e8fe240a58bb0de76d120a83a9d
dhokabeatz <amglna2020@gmail.com>
2023-05-12 11:04:41 -0700

    0-main.c bettied

--- 0x15-file_io/0-main.c
+++ 0x15-file_io/0-main.c
@@ -3,10 +3,10 @@
 #include "main.h"
 
 /**
- * main - check the code
- *
- * Return: Always 0.
- */
+* main - check the code
+*
+* Return: Always 0.
+*/
 int main(int ac, char **av)
 {
     ssize_t n;
